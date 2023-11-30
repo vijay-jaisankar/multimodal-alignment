9e1894cf2ab3b0ede33889cb2e510c57ba339d2c
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-11-30 14:47:49 +0530

    feat: load label npy file

---
+++ scripts/load-modelnet/show_labels_numpy.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+# Base directory path for the .npy files
+base_dir_path = "/home/vijay/Desktop/college/active-work/multimodal-alignment/data/modelnet40c-numpy/modelnet40_c"
+
+# Load npy file
+def load_npy(filename):
+    data = None
+    try:
+        data =  np.load(filename, allow_pickle=True) 
+    except Exception as e:
+        print(f"Error while loading {filename}: {e.with_traceback()}")
+        return data
+    return data
+
+if __name__ == "__main__":
+    # Load the labels file
+    label_filename = f"{base_dir_path}/label.npy"
+    data = np.array(load_npy(label_filename))
+    print(f"Shape of the data file: {data.shape}")
+
+    # Value counts
+    value_counts = np.array(np.unique(data, return_counts=True)).T
+    print(value_counts)
